8a24199cd3c67137a6990633c59ce1bded8e417d
evmgolf <evmgolf@github.com>
2022-05-30 13:00:57 +0100

    (fix) util now correctly etches the runtime instead of creation code, example now uses a code with storage and tests it.

--- script/Create2.sol
+++ script/Create2.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.13;
-import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
+import {Create2} from "create2/Create2.sol";
 import {Create2Deployer} from "src/Create2Deployer.sol";
 
-contract Create2Example is Script {
+contract Create2Example is Test {
   function run () external {
     (new Create2Deployer()).run();
+
     bytes memory _text = hex"00";
     uint salt = 0;
-    address to;
-
     vm.startBroadcast();
-    assembly {
-      to := create2(0, add(_text, 0x20), mload(_text), salt)
-    }
+    address to = Create2.create2Text(salt, _text);
+    assertTrue(to != address(0));
+    assertEq0(to.code, _text);
   }
 }
--- src/Create2Deployer.sol
+++ src/Create2Deployer.sol
@@ -6,7 +6,7 @@ contract Create2Deployer is Script {
   function run () external {
     vm.etch(
       0x4e59b44847b379578588920cA78FbF26c0B4956C,
-      hex"604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3"
+      hex"7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3"
     );
   }
 }
